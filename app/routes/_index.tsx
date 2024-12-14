3e1607d7d150a99b86f79bd0847038494312ad49
minamorl <minamorl@users.noreply.github.com>
2024-12-14 12:20:29 +0900

    style(routes): update title and description to use consistent casing and improve readability style(routes): adjust image sizes for GitHub and X links for better layout style(routes): modify heading size for better responsiveness on different screens style(routes): enhance social links section with additional links and formatting style(routes): change list structure for chat messages to improve semantics style(routes): add footer with copyright information for better site structure

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -2,13 +2,13 @@ import type { MetaFunction } from "@remix-run/node";
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "Minamorl" },
-    { name: "description", content: "Welcome to Minamorl's world!" },
+    { title: "minamorl" },
+    { name: "description", content: "Welcome to minamorl's world!" },
   ];
 };
 
-const GitHubLink = () => <img src="logos/github.png" width="100"/>
-const XLink = () => <img src="logos/x.png" width="40"/>
+const GitHubLink = () => <img src="logos/github.png" width="50"/>
+const XLink = () => <img src="logos/x.png" width="20"/>
 
 
 export default function Index() {
@@ -21,30 +21,40 @@ export default function Index() {
 
       {/* Main Content */}
       <div className="relative z-10 text-center w-full max-w-screen-xl mx-auto px-4">
-        <h1 className="text-left my-40 text-8xl font-bold text-gray-800">
+        <h1 className="text-left my-40 text-6xl lg:text-8xl font-bold text-gray-800">
           Hi! I&#39;m <br/><span className="text-pink-500">minamorl</span>!
         </h1>
 
-        <ul className="mt-6 space-y-4">
+        <div className="mt-6 space-y-4 text-left">
           <Chat>Hi! I'm minamorl!</Chat>
-          <Chat>This is my portfolio site. Let me explain...</Chat>
+          <Chat>This is my portfolio site. Let me explain about me...</Chat>
           <Chat>
-            <p>Here's some links for my socials</p>
-            <GitHubLink />
-            <XLink />
+            <p>Here's some links for my socials:</p>
+            <ul className="py-4 align-baseline	">
+              <li className="m-2 h-4"><a href="https://github.com/minamorl"><GitHubLink /></a></li>
+              <li className="m-2 h-4"><a href="https://x.com/__not_exists__"><XLink /></a></li>
+              <li className="m-2 h-4 text-pink-400 hover:text-pink-600"><a href="https://note.com/fumetsusha">note</a></li>
+            </ul>
+            For others, check my <a href="https://lit.link/notexists" className="text-pink-400 hover:text-pink-600">litlink</a>.
           </Chat>
-
-        </ul>
-
+          <Chat>
+            My main career is a software engineer. <br /> I'm interested in web app development.
+          </Chat>
+        </div>
       </div>
+      <footer>
+        <div className="text-center text-gray-500 text-sm py-4">
+          <p>© 2024 minamorl</p>
+        </div>
+      </footer>
     </div>
   );
 }
 
 const Chat: React.FC<React.PropsWithChildren> = ({ children }) => (
-  <li className="border-2 flex items-center space-x-4 rounded-xl bg-white px-4 py-8 ">
+  <div className="border-2 flex items-center space-x-4 rounded-xl bg-white px-4 py-8 ">
     <img src="avatar.png" alt="Avatar" className="w-8 h-8 rounded-full" />
-    <p className="text-lg font-medium text-gray-700">{children}</p>
-  </li>
+    <div className="text-lg font-medium text-gray-700">{children}</div>
+  </div>
 );
 
