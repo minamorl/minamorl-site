329384558f941ee402627d08523fd1efdf908d17
minamorl <minamorl@users.noreply.github.com>
2024-12-14 21:52:47 +0900

    feat(routes): add JobHistory component to showcase professional experience and key achievements
    This change enhances the user interface by providing a dedicated section for displaying job history, improving the overall presentation of professional qualifications.

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -13,6 +13,39 @@ export const meta: MetaFunction = () => {
 const GitHubLink = () => <img src="logos/github.png" alt="GitHub" width="50" />;
 const XLink = () => <img src="logos/x.png" alt="X" width="20" />;
 
+const JobHistory = () => (
+  <div className="relative z-10 text-left w-full max-w-screen-xl mx-auto px-4 my-8">
+    <h2 className="text-2xl lg:text-4xl font-bold text-gray-600">Professional Experience</h2>
+    <div className="mt-6 space-y-4">
+      <div className="border-2 flex flex-col space-y-2 rounded-xl bg-white px-4 py-4">
+        <h3 className="text-xl font-semibold text-gray-800">合同会社Trishula</h3>
+        <p className="text-gray-700">
+          <strong>Role:</strong> CTO<br />
+          <strong>Duration:</strong> September 2021 - January 2023<br />
+          <strong>Key Achievements:</strong>
+        </p>
+        <ul className="list-disc ml-8 text-gray-700">
+          <li>Led the development of a Reddit-like meta-internet platform.</li>
+          <li>Designed workflows and operated systems across all domains.</li>
+          <li>Implemented backend with TypeScript, Next.js, and Rust.</li>
+        </ul>
+      </div>
+      <div className="border-2 flex flex-col space-y-2 rounded-xl bg-white px-4 py-4">
+        <h3 className="text-xl font-semibold text-gray-800">株式会社クラスドゥ</h3>
+        <p className="text-gray-700">
+          <strong>Role:</strong> Full-stack Engineer & Data Scientist<br />
+          <strong>Duration:</strong> September 2020 - September 2021<br />
+          <strong>Key Achievements:</strong>
+        </p>
+        <ul className="list-disc ml-8 text-gray-700">
+          <li>Enhanced canvas rendering and stroke prediction in the frontend.</li>
+          <li>Improved PDF export processes for better efficiency.</li>
+        </ul>
+      </div>
+    </div>
+  </div>
+);
+
 export default function Index() {
   const [open, setOpen] = useState(true);
   return (
@@ -75,6 +108,7 @@ export default function Index() {
             </a>
             .
           </Chat>
+            <JobHistory />
         </div>
       </div>
       <footer>
